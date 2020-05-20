a060c52a341c69ab3dd46dff650e3ad5a5523312
SverxRazym <56333832+SverxRazym@users.noreply.github.com>
2020-05-20 23:57:09 +0400

    File js

--- index.php
+++ index.php
@@ -24,48 +24,7 @@
             </div>
         </div>
         <div class="container">
-            <div class="row padding">
-                <div class="col-md-4">
-                    <div class="card" id="Card">
-                        <img class="card-img-top" src="image/roflanLico.png" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Roflan</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card" id="Card">
-                        <img class="card-img-top"  width="100" src="image/roflanLico.png" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Roflan</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card" id="Card">
-                        <img class="card-img-top"  width="100" src="image/roflanLico.png" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Roflan</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card" id="Card">
-                        <img class="card-img-top"  width="100" src="image/roflanLico.png" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Roflan</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-
+            <div class="row padding" id="Container">
             </div>
 
         </div>
@@ -78,7 +37,7 @@
             </div>
         </footer>
         <!--Parser        -->
-
+        <script src="js/contentCreate"></script>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
---
+++ js/contentCreate.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    // let tempElem = document.getElementById("Container");
+    console.log(tempElem);
+    for (let i = 0; i < 19; i++) {
+        let temp = "Card" + i ;
+        // tempElem.innerHTML += createCard(temp,"Lorem Ipsum",temp);
+    }
+};
+
+
+function createCard(title,description,idCard){
+    let element = "<div class=\"col-md-4\" id='" + idCard + "'\n" +
+        "                    <div class=\"card\" id=\"Card\">\n" +
+        "                        <img class=\"card-img-top\"  width=\"100\" src=\"image/roflanLico.png\" alt=\"Card image cap\">\n" +
+        "                        <div class=\"card-body\">\n" +
+        "                            <h5 class=\"card-title\">"+ title +"</h5>\n" +
+        "                            <p class=\"card-text\">" + description + "</p>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>";
+    return element;
+}

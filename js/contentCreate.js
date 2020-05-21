921dc0e18076b8b1c40a7d49b93720dfec3845d1
SverxRazym <56333832+SverxRazym@users.noreply.github.com>
2020-05-21 11:13:59 +0400

    Lico krasnii

--- js/contentCreate.js
+++ js/contentCreate.js
@@ -1,15 +1,15 @@
 window.onload = function () {
-    // let tempElem = document.getElementById("Container");
+    let tempElem = document.getElementById("Container");
     console.log(tempElem);
     for (let i = 0; i < 19; i++) {
         let temp = "Card" + i ;
-        // tempElem.innerHTML += createCard(temp,"Lorem Ipsum",temp);
+        tempElem.innerHTML += createCard(temp,"Lorem Ipsum",temp);
     }
 };
 
 
 function createCard(title,description,idCard){
-    let element = "<div class=\"col-md-4\" id='" + idCard + "'\n" +
+    let element = "<div class=\"col-md-3 Card-Custom\" id='" + idCard + "'\n" +
         "                    <div class=\"card\" id=\"Card\">\n" +
         "                        <img class=\"card-img-top\"  width=\"100\" src=\"image/roflanLico.png\" alt=\"Card image cap\">\n" +
         "                        <div class=\"card-body\">\n" +
